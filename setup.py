35fde441f36cf02c6b25bc41500f9709b2fc2d6b
Daniel Jones <daniel@jones.org.uk>
2019-10-20 17:34:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+from distutils.core import setup
+
+setup(
+    name = 'generate-changelog-from-releases',
+    version = '0.0.1',
+    scripts = [ 'generate-changelog-from-releases.py' ]
+)
