a5ba3fbbd5fe312ee75e06ef85cc5f32419df1ed
Daniel Jones <daniel@jones.org.uk>
2019-10-20 17:36:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from distutils.core import setup
 
 setup(
     name = 'generate-changelog-from-releases',
+    description = 'Generate a CHANGELOG.md from GitHub releases',
     version = '0.0.1',
+    author = 'Daniel Jones',
+    author_email = 'dan-code@erase.net',
     scripts = [ 'generate-changelog-from-releases.py' ]
 )
